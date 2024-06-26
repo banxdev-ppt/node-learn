6c0d31d551d086ae92666d5685e8d4df95afb0bd
ponpitak <bankppt.srm@gmail.com>
2024-06-26 11:11:43 +0700

    update swagger ui

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 //services
 import loginAuth from './authentication/login';
 import registerAuth from './authentication/register';
 
 const app = express();
+const swaggerDocument = YAML.load(path.join(__dirname, '../src/swagger/swagger.yaml'));
+
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
@@ -15,6 +20,8 @@ dotenv.config();
 app.get('/', (req: Request, res: Response) => {
   res.send('server is running!');
 });
+
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.post('/login', loginAuth);
 app.post('/register', registerAuth);
